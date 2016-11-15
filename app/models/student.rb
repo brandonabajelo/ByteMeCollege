de4dce773332871128a8cb34ed0fde8262220c23
Nathan <nateinthegame@gmail.com>
2016-11-15 14:37:20 -0800

    Edit
    Removed drop down animation from the nav-bar and created a table to view
    user accounts

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
 	before_save { self.user_name = user_name.downcase }
 	before_save { self.email = email.downcase }
 	
-	validates :user_name, presence: true, length: { minimum: 8, maximum: 18 }, 
+	validates :user_name, presence: true, length: { minimum: 4, maximum: 18 }, 
 				uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 8, maximum: 18 }
 	validates :first_name, presence: true, length: { maximum: 50 }
