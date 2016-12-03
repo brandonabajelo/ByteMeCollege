daafa16b1a765df6606b334addaf71151c5785a4
Nathan <nateinthegame@gmail.com>
2016-12-02 17:41:17 -0800

    About us
    Linked in icons

--- app/assets/javascripts/javaScript.js
+++ app/assets/javascripts/javaScript.js
@@ -15,17 +15,3 @@ toggle between hiding and showing the dropdown content */
 		 }
 	 });
 });
-
-/* Add slideDown animation to dropdown */
-$(document).ready(function(){
-    $(".dropdown").hover(            
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
-            $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
-            $(this).toggleClass('open');       
-        }
-    );
-});  
